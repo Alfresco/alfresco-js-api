036b188a2642aa27a69e8fb555f1730dc722fb49
Will Abson <will.abson@gmail.com>
2016-05-06 13:15:50 +0100

    Remove test code from main.js

--- bundle.js
+++ bundle.js
@@ -8,25 +8,6 @@ global.AlfrescoApi = {
     NodesApi: NodesApi
 };
 
-var defaultClient = new AlfrescoCoreRestApi();
-defaultClient.basePath = 'http://192.168.99.100:8080/alfresco/api/-default-/public/alfresco/versions/1';
-
-// Configure HTTP basic authorization: basicAuth
-var basicAuth = defaultClient.authentications['basicAuth'];
-basicAuth.username = 'admin';
-basicAuth.password = 'admin';
-
-var apiInstance = new NodesApi(defaultClient);
-
-var nodeId = '-root-', opts = {};
-var callback = function(error, data, response) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('API called successfully. Returned data: ', data);
-    }
-};
-apiInstance.getNodeChildren(nodeId, opts, callback);
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./src/ApiClient.js":5,"./src/api/NodesApi.js":6}],2:[function(require,module,exports){
@@ -1887,7 +1868,7 @@ module.exports = function(arr, fn, initial){
 }));
 
 }).call(this,require("buffer").Buffer)
-},{"buffer":27,"fs":26,"superagent":2}],6:[function(require,module,exports){
+},{"buffer":28,"fs":27,"superagent":2}],6:[function(require,module,exports){
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
