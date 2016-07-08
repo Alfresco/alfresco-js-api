976c963404c48f89b74603fe42432379c44a70ff
Eugenio Romano <eugenio.romano@alfresco.com>
2016-07-08 18:56:24 +0100

    configurable host in content mock

--- test/contet.spec.js
+++ test/contet.spec.js
@@ -17,7 +17,7 @@ describe('Alfresco Content', function () {
             host: this.host
         });
 
-        this.alfrescoJsApi.login().then((data) => {
+        this.alfrescoJsApi.login().then(() => {
             done();
         });
     });
@@ -25,7 +25,7 @@ describe('Alfresco Content', function () {
     it('get Document Thumbnail Url', function () {
         var thumbnailUrl = this.alfrescoJsApi.content.getDocumentThumbnailUrl('1a0b110f-1e09-4ca2-b367-fe25e4964a4');
 
-        expect(thumbnailUrl).to.be.equal('http://127.0.0.1:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/' +
+        expect(thumbnailUrl).to.be.equal(this.host + '/alfresco/api/-default-/public/alfresco/versions/1/nodes/' +
             '1a0b110f-1e09-4ca2-b367-fe25e4964a4/renditions/doclib/content?attachment=false&' +
             'alf_ticket=TICKET_4479f4d3bb155195879bfbb8d5206f433488a1b1');
     });
@@ -33,7 +33,7 @@ describe('Alfresco Content', function () {
     it('get Content Url', function () {
         var contentUrl = this.alfrescoJsApi.content.getContentUrl('1a0b110f-1e09-4ca2-b367-fe25e4964a4');
 
-        expect(contentUrl).to.be.equal('http://127.0.0.1:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/' +
+        expect(contentUrl).to.be.equal(this.host + '/alfresco/api/-default-/public/alfresco/versions/1/nodes/' +
             '1a0b110f-1e09-4ca2-b367-fe25e4964a4/content?attachment=false' +
             '&alf_ticket=TICKET_4479f4d3bb155195879bfbb8d5206f433488a1b1');
     });
