11d4148b19d4f258a89778e268e138b579764731
Eugenio Romano <eugenio.romano@alfresco.com>
2016-07-13 17:57:19 +0100

    move webScriptMock file

--- test/mockObjects/mockAlfrescoApi.js
+++ test/mockObjects/mockAlfrescoApi.js
@@ -3,6 +3,6 @@ var mockAlfrescoApi = {};
 mockAlfrescoApi.Auth = require('./authResponseMock.js');
 mockAlfrescoApi.Node = require('./nodeMock.js');
 mockAlfrescoApi.Upload = require('./uploadMock.js');
-mockAlfrescoApi.WebScript= require('./webScriptMock.js');
+mockAlfrescoApi.WebScript = require('./webScriptMock.js');
 
 module.exports = mockAlfrescoApi;
