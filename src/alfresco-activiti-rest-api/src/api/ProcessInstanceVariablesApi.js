dbbe43f45a2fb1393ee1cb99e2d423214b6428f3
Eugenio Romano <eugenio.romano@alfresco.com>
2016-12-05 12:52:17 +0000

    #111 fix webpack import for ProcessInstanceVariables and add a test to raise an error for future build

--- src/alfresco-activiti-rest-api/src/api/ProcessInstanceVariablesApi.js
+++ src/alfresco-activiti-rest-api/src/api/ProcessInstanceVariablesApi.js
@@ -1,7 +1,7 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/RestVariable'], factory);
+    define(['../../../alfrescoApiClient', '../model/RestVariable'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
     module.exports = factory(require('../../../alfrescoApiClient'), require('../model/RestVariable'));
