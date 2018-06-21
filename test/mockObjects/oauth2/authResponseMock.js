0c471fa5f412ce95a921783444e7e066a913d66d
Eugenio Romano <eugenio.romano@alfresco.com>
2018-06-21 20:22:58 +0100

    fix test and build

--- test/mockObjects/oauth2/authResponseMock.js
+++ test/mockObjects/oauth2/authResponseMock.js
@@ -12,23 +12,29 @@ class AuthResponseMock extends BaseMock {
     }
 
     get200Response() {
-        nock(this.host, {'encodedQueryParams': true})
-            .post('/oauth/token')
-            .query({'username': this.username, 'password': this.password, 'grant_type': 'password'})
+        nock('http://myOauthUrl:30081', {"encodedQueryParams": true})
+            .post('/auth/realms/springboot/protocol/openid-connect/token/')
             .reply(200, {
-                'access_token': '5c37e781-40a7-4957-adcc-2b171c770a5c',
-                'token_type': 'bearer',
-                'refresh_token': '15d66b26-3cf7-446a-8db8-1345f2f4485a',
-                'expires_in': 604603,
-                'scope': 'openid',
-                'legacyToken': 'LegacyToken{legacyTokenBpm=\'Basic YWRtaW46YWRtaW4=\', tokenId=\'null\', legacyTokenEcm=\'TICKET_bbead3a54dbe141f77e442e6703f7fa29671107a\'}'
+                "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0cHczOUltNE54dXZEN0ZqZ3JOQ3Q2ZkpaVDctQ3JWTzRkX3BLaXJlSTF3In0.eyJqdGkiOiI2OWQ2Njk4Ni0zMWIxLTQzMDYtYjQ0ZS0wZTZiOGFiNmMwMTUiLCJleHAiOjE1Mjk2MDc2MDYsIm5iZiI6MCwiaWF0IjoxNTI5NjA3MzA2LCJpc3MiOiJodHRwOi8vYTVlMmY5M2RlMTBhZjExZThhMDU2MGExYmVhNWI3YzgtMjM2NzA5NDMzLnVzLWVhc3QtMS5lbGIuYW1hem9uYXdzLmNvbTozMDA4MS9hdXRoL3JlYWxtcy9zcHJpbmdib290IiwiYXVkIjoiYWN0aXZpdGkiLCJzdWIiOiJlMjRjZjM0Mi1mYzUwLTRjYjEtYTBjMC01N2RhZWRiODI3NDkiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhY3Rpdml0aSIsImF1dGhfdGltZSI6MCwic2Vzc2lvbl9zdGF0ZSI6Ijk0MzNlMjA2LWRmMWEtNDZhMy1iZTdlLTY1YjA0NWExYzM2YiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDozMDAwKiIsImh0dHA6Ly9hY3Rpdml0aS1jbG91ZC1kZW1vLXVpOjMwMDAiLCJodHRwOi8vYXBza2V5Y2xvYWtzMy5lbnZhbGZyZXNjbyoiLCJodHRwOi8vYXBza2V5Y2xvYWsuZW52YWxmcmVzY28uY29tIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJhZG1pbiIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsicmVhbG0tbWFuYWdlbWVudCI6eyJyb2xlcyI6WyJ2aWV3LWlkZW50aXR5LXByb3ZpZGVycyIsInZpZXctcmVhbG0iLCJtYW5hZ2UtaWRlbnRpdHktcHJvdmlkZXJzIiwiaW1wZXJzb25hdGlvbiIsInJlYWxtLWFkbWluIiwiY3JlYXRlLWNsaWVudCIsIm1hbmFnZS11c2VycyIsInF1ZXJ5LXJlYWxtcyIsInZpZXctYXV0aG9yaXphdGlvbiIsInF1ZXJ5LWNsaWVudHMiLCJxdWVyeS11c2VycyIsIm1hbmFnZS1ldmVudHMiLCJtYW5hZ2UtcmVhbG0iLCJ2aWV3LWV2ZW50cyIsInZpZXctdXNlcnMiLCJ2aWV3LWNsaWVudHMiLCJtYW5hZ2UtYXV0aG9yaXphdGlvbiIsIm1hbmFnZS1jbGllbnRzIiwicXVlcnktZ3JvdXBzIl19LCJicm9rZXIiOnsicm9sZXMiOlsicmVhZC10b2tlbiJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW4ifQ.mJd_FFUor0KydllQWQ-MHprQYH6xman_Tu3054bBdcMT2Q4JmduTzucJBe0gUJkTJfr6xZ9dVfhHn6Gbw-bumwczLq2tzosIN6XipMwLRWSxxsApkDuQq-jwuOnO8bigQO8X1mBJGxenLsJueYjFP3aQlhWvJ0Seuq-I5-dCQ_tofbb3XqicJyz9OmsuJ3aZYHBWryQVEXfoadgFDMwTZHpQ00GH4JzTI4jZVbrG7SgkpZCSTrNGLNAF3bcdN_YWzKxw9RHZ03nOCVRy9j0_o49RoNmB8O2PvrVeqV49H8bRTYGCmkNyQwFQL2HNvvXUVxigqw6XkDDqZoEWIA5DAQ",
+                "expires_in": 300,
+                "refresh_expires_in": 1800,
+                "refresh_token": "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0cHczOUltNE54dXZEN0ZqZ3JOQ3Q2ZkpaVDctQ3JWTzRkX3BLaXJlSTF3In0.eyJqdGkiOiI2MzQxMDc1ZC1lOTY4LTRmZTctOTkwZS05MTQ3NTUwOGEzZWIiLCJleHAiOjE1Mjk2MDkxMDYsIm5iZiI6MCwiaWF0IjoxNTI5NjA3MzA2LCJpc3MiOiJodHRwOi8vYTVlMmY5M2RlMTBhZjExZThhMDU2MGExYmVhNWI3YzgtMjM2NzA5NDMzLnVzLWVhc3QtMS5lbGIuYW1hem9uYXdzLmNvbTozMDA4MS9hdXRoL3JlYWxtcy9zcHJpbmdib290IiwiYXVkIjoiYWN0aXZpdGkiLCJzdWIiOiJlMjRjZjM0Mi1mYzUwLTRjYjEtYTBjMC01N2RhZWRiODI3NDkiLCJ0eXAiOiJSZWZyZXNoIiwiYXpwIjoiYWN0aXZpdGkiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiI5NDMzZTIwNi1kZjFhLTQ2YTMtYmU3ZS02NWIwNDVhMWMzNmIiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiYWRtaW4iLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7InJlYWxtLW1hbmFnZW1lbnQiOnsicm9sZXMiOlsidmlldy1pZGVudGl0eS1wcm92aWRlcnMiLCJ2aWV3LXJlYWxtIiwibWFuYWdlLWlkZW50aXR5LXByb3ZpZGVycyIsImltcGVyc29uYXRpb24iLCJyZWFsbS1hZG1pbiIsImNyZWF0ZS1jbGllbnQiLCJtYW5hZ2UtdXNlcnMiLCJxdWVyeS1yZWFsbXMiLCJ2aWV3LWF1dGhvcml6YXRpb24iLCJxdWVyeS1jbGllbnRzIiwicXVlcnktdXNlcnMiLCJtYW5hZ2UtZXZlbnRzIiwibWFuYWdlLXJlYWxtIiwidmlldy1ldmVudHMiLCJ2aWV3LXVzZXJzIiwidmlldy1jbGllbnRzIiwibWFuYWdlLWF1dGhvcml6YXRpb24iLCJtYW5hZ2UtY2xpZW50cyIsInF1ZXJ5LWdyb3VwcyJdfSwiYnJva2VyIjp7InJvbGVzIjpbInJlYWQtdG9rZW4iXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX19.mQ4Vi1yLG9KvcmmhHlgOowy8D30iaUsiO7--JTPY7Ol-R1eY4wvRn1cH5FllieXk8yltYGP23xXNtTC4M54guXGVtgRgo8AlRklFHL1BMlxpa0OPwcNmwthx1-P2n7c9XL1e8pt2uRhQJLxunr2TpLaQi0UpEmEyouXHfR7sxM1AzKAf3b9Nk7f7lrk__2BYlFsL3YcGlFDqDMgPfhNlDbR-rQGoxlOjt0YqS8ktYq4bneL5etpXnPh0oEt4B7FFK-WKKuOWR6rQ9791ACnn6puz6C_Ki261IkZ0a_Uu7tOA4Xi9xzoQKLgSTAlBeg4u86Wb5kjL5r2-3zTg-Dikew",
+                "token_type": "bearer",
+                "not-before-policy": 0,
+                "session_state": "9433e206-df1a-46a3-be7e-65b045a1c36b"
             });
+
     }
 
     get200CustomResponse(path) {
-        nock(this.host, {'encodedQueryParams': true})
+        nock('http://myOauthUrl:30081/auth/realms/springboot/protocol/', {'encodedQueryParams': true})
             .post(path)
-            .query({'username': this.username, 'password': this.password, 'grant_type': 'password'})
+            .query({
+                'username': this.username,
+                'password': this.password,
+                'grant_type': 'password',
+                'client_id': 'alfrescoapp'
+            })
             .reply(200, {
                 'access_token': '5c37e781-40a7-4957-adcc-2b171c770a5c',
                 'token_type': 'bearer',
@@ -40,8 +46,8 @@ class AuthResponseMock extends BaseMock {
     }
 
     get200RefreshTokenResponse(refreshToken) {
-        nock(this.host, {'encodedQueryParams': true})
-            .post('/oauth/token')
+        nock('http://myOauthUrl:30081', {"encodedQueryParams": true})
+            .post('/auth/realms/springboot/protocol/openid-connect/token/')
             .query({
                 refresh_token: refreshToken,
                 grant_type: 'refresh_token'
