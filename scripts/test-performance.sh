0ee835f617664cf541df6ed9f6c2ad887c5becdb
Eugenio Romano <eugenio.romano@alfresco.com>
2019-01-09 16:28:08 +0000

    fix performance test

--- scripts/test-performance.sh
+++ scripts/test-performance.sh
@@ -12,4 +12,8 @@ npm run build:prod || exit 1
 #Test webpack build size
 cd $DIR/../test/performance/test-webpack
 npm install
+
+mkdir -p $DIR/../test/performance/test-webpack/node_modules/@alfresco/js-api
+cp -R $DIR/../dist/package/* $DIR/../test/performance/test-webpack/node_modules/@alfresco/js-api
+
 npm run build || exit 1
