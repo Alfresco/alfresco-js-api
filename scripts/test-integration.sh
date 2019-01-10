9e8c62c1850545127b298ddf6158dee33fd6f332
Eugenio Romano <eugenio.romano@alfresco.com>
2019-01-10 10:15:59 +0000

    fix integration test script

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -4,5 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
 
-ts-node ./test/integration/test-node/index.ts --host $HOST -u "$USERNAME" -p '$PASSWORD' || exit 1
-ts-node ./test/integration/test-node/upload-file.ts --host $HOST -u "$USERNAME" -p '$PASSWORD' || exit 1
+mkdir -p $DIR/../test/integration/test-node/node_modules/@alfresco/js-api
+cp -R $DIR/../dist/package/* $DIR/../test/integration/test-node/node_modules/@alfresco/js-api
+
+ts-node ./test/integration/test-node/index.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
+ts-node ./test/integration/test-node/upload-file.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
