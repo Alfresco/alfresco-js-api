e72fe443daedba4b99f898d6a15012e3b7c629e9
Eugenio Romano <eugenio.romano@alfresco.com>
2019-10-16 17:59:32 +0100

    move release on travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,26 @@ cd $DIR/..
 rm -rf package/
 rm -rf node_modules/@alfresco/js-api/
 
+npm install @alfresco/adf-cli@alpha
+
+if [[ $TRAVIS_PULL_REQUEST == "false" ]];
+then
+    if [[ $TRAVIS_BRANCH == "development" ]];
+    then
+        NEXT_VERSION=-nextalpha
+        if [[ $TRAVIS_EVENT_TYPE == "cron" ]];
+        then
+            NEXT_VERSION=-nextbeta
+        fi
+        ./scripts/update-version.sh -gnu $NEXT_VERSION || exit 1;
+    fi
+
+    node ./scripts/pre-publish.js
+
+else
+    ./node_modules/@alfresco/adf-cli/bin/adf-cli update-version --alpha --pathPackage "$(pwd)"
+fi;
+
+echo "====== Build ====="
 npm run build_all
+
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,25 +4,31 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
 
-TAG="latest"
+if [[ $TRAVIS_PULL_REQUEST == "false" ]];
+then
+    TAG_NPM=latest
+    if [[ $TRAVIS_BRANCH == "development" ]];
+    then
+        TAG_NPM=alpha
+        if [[ $TRAVIS_EVENT_TYPE == "cron" ]];
+        then
+            TAG_NPM=beta
+        fi
+    fi
 
-show_help() {
-    echo "Usage: publish.sh"
-    echo "-t or --tag Sets tag `latest` if no --tag specified "
-    echo ""
-}
+    echo "Publishing on npm with tag $TAG_NPM"
 
-tag_assign() {
-   TAG=$1
-}
+    touch ./dist/package/.npmrc
+    echo 'strict-ssl=false' >> ./dist/package/.npmrc
+    echo 'registry=http://${NPM_REGISTRY_ADDRESS}' >> ./dist/package/.npmrc
+    echo '//${bamboo.npmregistry_address}/:_authToken="${NPM_REGISTRY_TOKEN}"' >> ./dist/package/.npmrc
 
-while [[ $1 == -* ]]; do
-    case "$1" in
-      -t|--tag)  tag_assign $2; shift 2;;
-      -*) echo "invalid option: $1" 1>&2; show_help; exit 1;;
-    esac
-done
+    cd dist/package/
+    npm publish --tag ${TAG}
 
-cd dist/package/
-npm publish --tag ${TAG}
-cd ../../
+    rm -rf .npmrc
+
+    cd ../../
+
+fi;
+
