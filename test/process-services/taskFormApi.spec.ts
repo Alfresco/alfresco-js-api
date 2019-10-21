a830cfbd38f9daa88b771fe5b8e62e3ccc42fa1c
Denys Vuika <denys.vuika@alfresco.com>
2019-10-21 14:59:52 +0100

    upgrade to latest superagent and nock libs (#450)

--- test/process-services/taskFormApi.spec.ts
+++ test/process-services/taskFormApi.spec.ts
@@ -38,12 +38,14 @@ describe('Activiti Task Api', function () {
         this.taskFormMock.get200getTaskFormVariables();
 
         let taskId = 5028;
-        this.alfrescoJsApi.activiti.taskFormsApi.getTaskFormVariables(taskId).then(() => {
-            expect(this.alfrescoJsApi.activiti.taskFormsApi.apiClient.authentications.cookie).equal('ACTIVITI_REMEMBER_ME=NjdOdGwvcUtFTkVEczQyMGh4WFp5QT09OmpUL1UwdFVBTC94QTJMTFFUVFgvdFE9PQ');
-            done();
-        },                                                                         (error: any) => {
-            console.log('error' + error);
-        });
+        this.alfrescoJsApi.activiti.taskFormsApi.getTaskFormVariables(taskId).then(
+            () => {
+                expect(this.alfrescoJsApi.activiti.taskFormsApi.apiClient.authentications.cookie[0]).equal('ACTIVITI_REMEMBER_ME=NjdOdGwvcUtFTkVEczQyMGh4WFp5QT09OmpUL1UwdFVBTC94QTJMTFFUVFgvdFE9PQ');
+                done();
+            },
+            (error: any) => {
+                console.log('error' + error);
+            });
     });
 
 });
