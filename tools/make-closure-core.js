d6e0f3edf40c08b22f88505e81bb3e8364e2dc26
Denys Vuika <denys.vuika@gmail.com>
2019-10-22 14:33:27 +0100

    migrate to rollup closure compiler plugin

--- tools/make-closure-core.js
+++ tools/make-closure-core.js
@@ -1,3 +1,7 @@
-var makeClosure = require('./make-closure');
+// const rollupBundle = require('./rollup-bundle');
 
-makeClosure('dist/global/alfresco-js-api.umd.js');
+// rollupBundle({
+//     input: 'dist/esm5_for_rollup/src/internal/umd.js',
+//     dest: 'dist/global/alfresco-js-api.umd.min.js',
+//     minify: true
+// });
