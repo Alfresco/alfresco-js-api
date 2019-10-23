835440dc489ee43cb780b4fa16964dadff91645c
Denys Vuika <denys.vuika@alfresco.com>
2019-10-23 09:47:54 +0100

    [ADF-4974] security fixes (#455)
    * upgrade test-webpack project
    
    * remove unused angular libs
    
    * remove the lite-server dependency
    
    * upgrade to angular 8
    
    * update test libs
    
    * update libs
    
    * final security updates for angular app
    
    * bump node version
    
    * upgrade to latest rollup

--- tools/rollup-bundle.js
+++ tools/rollup-bundle.js
@@ -4,52 +4,42 @@ const alias = require('rollup-plugin-alias');
 const inject = require('rollup-plugin-inject');
 const resolve = require('rollup-plugin-node-resolve');
 const compiler = require('@ampproject/rollup-plugin-closure-compiler');
-const fs = require('fs');
 const tslib = require('tslib');
-const path = require('path');
 
-module.exports = function rollupBundle(options) {
+module.exports = async function rollupBundle(options) {
     const dest = options.dest;
-    const sourcemapFullFile = dest + '.map';
 
-    rollup
-        .rollup({
-            input: options.input,
-            plugins: [
-                alias(options.aliases),
-                resolve(),
-                inject({
-                    exclude: 'node_modules/**',
-                    modules: _.mapValues(tslib, function(value, key) {
-                        return ['tslib', key];
-                    })
-                }),
-                options.minify ? compiler() : undefined
-            ]
-        })
-        .then(function(bundle) {
-            return bundle.generate({
-                format: 'umd',
-                name: '@alfresco/js-api',
-                amd: {
-                    id: '@alfresco/js-api'
-                },
-                sourcemap: true
-            });
-        })
-        .then(function(result) {
-            // rollup doesn't add a sourceMappingURL
-            // https://github.com/rollup/rollup/issues/121
-            result.code =
-                result.code +
-                '\n//# sourceMappingURL=' +
-                path.basename(sourcemapFullFile);
+    const inputOptions = {
+        input: options.input,
+        plugins: [
+            alias(options.aliases),
+            resolve(),
+            inject({
+                exclude: 'node_modules/**',
+                modules: _.mapValues(tslib, function(value, key) {
+                    return ['tslib', key];
+                })
+            }),
+            options.minify ? compiler() : undefined
+        ]
+    };
 
-            fs.writeFileSync(dest, result.code);
-            fs.writeFileSync(sourcemapFullFile, result.map);
-        })
-        .catch(function(err) {
-            console.error(err);
-            process.exit(1);
-        });
+    const outputOptions = {
+        format: 'umd',
+        name: '@alfresco/js-api',
+        file: dest,
+        amd: {
+            id: '@alfresco/js-api'
+        },
+        sourcemap: true,
+        sourcemapFile: `${dest}.map`
+    }
+
+    try {
+        const bundle = await rollup.rollup(inputOptions);
+        await bundle.write(outputOptions);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 };
