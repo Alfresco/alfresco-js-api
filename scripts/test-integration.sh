8039204e1b5750c615115ec2025b3e2e65ac8039
Eugenio Romano <eugenio.romano@alfresco.com>
2019-11-05 07:49:52 +0000

    add integration test for SSO

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -9,6 +9,7 @@ cat dist/package/package.json | grep version
 mkdir -p $DIR/../test/integration/test-node/node_modules/@alfresco/js-api
 cp -R $DIR/../dist/package/* $DIR/../test/integration/test-node/node_modules/@alfresco/js-api
 
-ts-node ./test/integration/test-node/index.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
+ts-node ./test/integration/test-node/login-ecm.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
 ts-node ./test/integration/test-node/login-bpmn.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
 ts-node ./test/integration/test-node/upload-file.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
+ts-node ./test/integration/test-node/login-sso.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" -ssoHost "${SSO_HOST}" -clientId "${CLIENT_ID}" || exit 1
