32921435e8364abe1bce57b935b6f5f37ea4ee01
eromano <eugenioromano16@gmail.com>
2019-11-25 12:20:17 +0000

    fix url

--- scripts/git-tag.sh
+++ scripts/git-tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[  $TRAVIS_BRANCH == "master" ]]; then
-    TAG_VERSION=$(grep -m1 version package.json | awk '{ print $2 }' | sed 's/[", ]//g')
+    TAG_VERSION=$(npm view @alfresco/js-api@beta version)
 else
     TAG_VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
 fi;
@@ -9,10 +9,10 @@ fi;
 echo $TAG_VERSION
 
 echo "git tag -a ${VERSION} -m ${VERSION}"
-git config --local user.name "alfresco-build"
-git config --local user.email "build@alfresco.com"
+git config --local user.name "eromano"
+git config --local user.email "eugenioromano16@gmail.com"
 git tag -a ${VERSION} -m "${VERSION} [ci skip] "
 git remote rm origin
-GITHUB_REPO=https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-ng2-components.git
+GITHUB_REPO=https://$GITHUB_TOKEN:x-oauth-basic@github.com/Alfresco/alfresco-js-api.git
 git remote add origin $GITHUB_REPO
 git push origin --tags
