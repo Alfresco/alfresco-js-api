232d068faf4157397e95a7fec59425196c93052a
Denys Vuika <denys.vuika@alfresco.com>
2020-01-28 12:54:41 +0000

    remove console.log from the auth (#585)

--- src/authentication/processAuth.ts
+++ src/authentication/processAuth.ts
@@ -40,8 +40,6 @@ export class ProcessAuth extends AlfrescoApiClient {
         this.className = 'ProcessAuth';
 
         if (!this.isBrowser()) {
-            console.log('this.defaultHeaders() ' + this.defaultHeaders);
-
             this.defaultHeaders = {
                 'user-agent': 'alfresco-js-api'
             };
