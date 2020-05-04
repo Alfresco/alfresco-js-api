a3e8f881a5cdafdb3f228f9ce3827dab8f3dc9ef
eromano <eugenioromano16@gmail.com>
2020-05-04 11:36:24 +0100

    better script release

---
+++ scripts/update-project.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -e
+TEMP_GENERATOR_DIR=".tmp-generator";
+VERSION=$(npm view @alfresco/adf-core@beta version)
+JS_VERSION=$(npm view @alfresco/js-api@beta version)
+
+show_help() {
+    echo "Usage: update-project.sh"
+    echo ""
+    echo "-t or --token  Github ouath token"
+    echo "-n or --name  Github name of the project"
+    echo "-v or --version  ADF version if not passed will use the beta"
+    echo "-vjs or --vjs  JS API version if not passed will use the beta"
+}
+
+token() {
+    TOKEN=$1
+}
+
+vjs() {
+    JS_VERSION=$1
+}
+
+version() {
+    VERSION=$1
+}
+
+name_repo() {
+    NAME_REPO=$1
+}
+
+while [[ $1 == -* ]]; do
+    case "$1" in
+      -h|--help|-\?) show_help; exit 0;;
+      -n|--name|-\?)  name_repo $2; shift 2;;
+      -t|--token)  token $2; shift 2;;
+      -v|--version)  version $2; shift 2;;
+      -vjs|--vjs)  vjs $2; shift 2;;
+      -*) echo "invalid option: $1" 1>&2; show_help; exit 1;;
+    esac
+done
+
+rm -rf $TEMP_GENERATOR_DIR;
+
+git clone https://$TOKEN@github.com/$NAME_REPO.git $TEMP_GENERATOR_DIR
+cd $TEMP_GENERATOR_DIR
+git checkout development
+
+BRANCH="JS-API-Update-$VERSION"
+git checkout -b $BRANCH
+
+./scripts/update-version.sh -gnu -v $VERSION -vj $JS_VERSION
+
+git add .
+git commit -m "Update JS-API packages version $VERSION"
+git push -u origin $BRANCH
+
+curl -H "Authorization: token $TOKEN" -X POST -d '{"body":"Update JS-API packages version '$VERSION'","head":"'$BRANCH'","base":"development","title":"Update JS-API packages version '$VERSION'"}' https://api.github.com/repos/$NAME_REPO/pulls
+
+rm -rf $TEMP_GENERATOR_DIR;
