caed9cd2e474f697c28ef202e32ffa4af2f9c68d
ursedaniel <daniel.urse@hotmail.com>
2020-06-11 12:49:19 +0300

    update deprecated code

--- test/content-services/versionsApi.spec.ts
+++ test/content-services/versionsApi.spec.ts
@@ -1,5 +1,9 @@
 /*global describe, it, beforeEach */
 
+import { AlfrescoApi } from '../../src/alfrescoApi';
+import { VersionsApi } from '../../src/api/content-rest-api';
+
+const AuthResponseMock = require('../../test/mockObjects/mockAlfrescoApi').Auth;
 let VersionMock = require('../../test/mockObjects/mockAlfrescoApi').Version;
 let expect = require('chai').expect;
 
@@ -9,51 +13,66 @@ describe('Versions', function () {
     let versionId = '1.0';
     let renditionId = 'pdf';
 
-    beforeEach(function () {
-        let alfrescoJsApi = new AlfrescoApi({hostEcm: 'http://127.0.0.1:8080'});
+    let authResponseMock: any;
+    let versionMock: any;
+    let versionsApi: VersionsApi;
 
-        this.versionsApi = alfrescoJsApi.core.versionsApi;
-        this.versionMock = new VersionMock();
+    beforeEach((done) => {
+        const hostEcm = 'http://127.0.0.1:8080';
+
+        authResponseMock = new AuthResponseMock(hostEcm);
+        versionMock = new VersionMock();
+        authResponseMock.get201Response();
+
+        const alfrescoJsApi = new AlfrescoApi({
+                                                  hostEcm
+                                              });
+
+        alfrescoJsApi.login('admin', 'admin').then(() => {
+            done();
+        });
+
+        versionsApi = new VersionsApi(alfrescoJsApi);
     });
 
     it("should list all node's version renditions", function (done) {
-        this.versionMock.get200ResponseVersionRenditions(nodeId, versionId);
+        versionMock.get200ResponseVersionRenditions(nodeId, versionId);
 
-        this.versionsApi.listVersionRenditions(nodeId, versionId).then(function (data: any) {
+        versionsApi.listVersionRenditions(nodeId, versionId).then(function (data: any) {
             let entries = data.list.entries;
             expect(entries.length).to.be.equal(6);
             expect(data.list.entries[0].entry.id).to.be.equal('avatar');
             done();
-        },                                                             (err: any) => {
+        },                                                        (err: any) => {
             throw new Error(err);
         });
     });
 
     it("should create rendition for a node's versionId", function (done) {
-        this.versionMock.create200VersionRendition(nodeId, versionId);
+        versionMock.create200VersionRendition(nodeId, versionId);
 
-        this.versionsApi.createVersionRendition(nodeId, versionId, {id: 'pdf'}).then(function () {
+        versionsApi.createVersionRendition(nodeId, versionId, {id: 'pdf'}).then(function () {
             done();
-        },                                            (err: any) => {
+        },                                                                      (err: any) => {
             throw new Error(err);
         });
     });
 
     it("should get a node's version rendition", function (done) {
-        this.versionMock.get200VersionRendition(nodeId, versionId, renditionId);
+        versionMock.get200VersionRendition(nodeId, versionId, renditionId);
 
-        this.versionsApi.getVersionRendition(nodeId, versionId, renditionId).then(function (data: any) {
+        versionsApi.getVersionRendition(nodeId, versionId, renditionId).then(function (data: any) {
             expect(data.entry.id).to.be.equal('pdf');
             done();
-        },                                                             (err: any) => {
+        },                                                                   (err: any) => {
             throw new Error(err);
         });
     });
 
     it('should get version history', function (done) {
-        this.versionMock.get200Response(nodeId);
+        versionMock.get200Response(nodeId);
 
-        this.versionsApi.listVersionHistory(nodeId).then(function (data: any) {
+        versionsApi.listVersionHistory(nodeId).then(function (data: any) {
             let entries = data.list.entries;
             expect(entries.length).to.be.equal(2);
             expect(entries[0].entry.id).to.be.equal('2.0');
@@ -63,12 +82,12 @@ describe('Versions', function () {
     });
 
     it('should revert a version', function (done) {
-        this.versionMock.post201Response(nodeId, versionId);
+        versionMock.post201Response(nodeId, versionId);
 
-        this.versionsApi.revertVersion(nodeId, versionId, {majorVersion: true, comment: ''}).then(function (data: any) {
+        versionsApi.revertVersion(nodeId, versionId, {majorVersion: true, comment: ''}).then(function (data: any) {
             expect(data.entry.id).to.be.equal('3.0');
             done();
-        },                                                                                        function () {
+        },                                                                                   function () {
         });
     });
 });
