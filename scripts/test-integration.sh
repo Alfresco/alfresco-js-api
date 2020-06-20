d56d3881a6b0df7ce36367e0823300bf86012e88
Denys Vuika <denys.vuika@gmail.com>
2020-06-20 12:59:52 +0100

    fix scripts

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -4,10 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/..
 
-cat dist/package/package.json | grep version
+cat dist/package.json | grep version
 
 mkdir -p $DIR/../test/integration/test-node/node_modules/@alfresco/js-api
-cp -R $DIR/../dist/package/* $DIR/../test/integration/test-node/node_modules/@alfresco/js-api
+cp -R $DIR/../dist/* $DIR/../test/integration/test-node/node_modules/@alfresco/js-api
 
 ts-node ./test/integration/test-node/login-ecm.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
 ts-node ./test/integration/test-node/login-bpmn.ts --host $HOST -u "$USERNAME" -p "$PASSWORD" || exit 1
