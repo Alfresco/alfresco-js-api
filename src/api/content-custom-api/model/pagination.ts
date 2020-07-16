d71e8e87a15a822c2a024e5f258dc862daa9e066
Denys Vuika <denys.vuika@gmail.com>
2020-07-16 20:42:05 +0100

    remove duplicate pagination

--- src/api/content-custom-api/model/pagination.ts
+++ src/api/content-custom-api/model/pagination.ts
@@ -15,6 +15,8 @@
 * limitations under the License.
 */
 
+import { Pagination } from '../../content-rest-api/model/pagination';
+
 export interface PaginatedList<T> {
     list: {
         entries: Array<{ entry: T }>;
@@ -25,46 +27,4 @@ export interface PaginatedList<T> {
 export interface PaginatedEntries<T> {
     entries: T[];
     pagination: Pagination;
-}
-
-export class Pagination {
-    /**
-     * The number of objects in the entries array.
-
-     */
-    count?: number;
-    /**
-     * A boolean value which is **true** if there are more entities in the collection
-     beyond those in this response. A true value means a request with a larger value
-     for the **skipCount** or the **maxItems** parameter will return more entities.
-
-     */
-    hasMoreItems?: boolean;
-    /**
-     * An integer describing the total number of entities in the collection.
-     The API might not be able to determine this value,
-     in which case this property will not be present.
-
-     */
-    totalItems?: number;
-    /**
-     * An integer describing how many entities exist in the collection before
-     those included in this list. If there was no **skipCount** parameter then the
-     default value is 0.
-
-     */
-    skipCount?: number;
-    /**
-     * The value of the **maxItems** parameter used to generate this list.
-     If there was no **maxItems** parameter then the default value is 100.
-
-     */
-    maxItems?: number;
-
-    constructor(input?: any) {
-        if (input) {
-            Object.assign(this, input);
-        }
-    }
-
 }
