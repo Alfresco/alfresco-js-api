a4dde187682d829ca1b4cb7499b798f56a31a668
Denys Vuika <denys.vuika@gmail.com>
2021-05-28 15:49:00 +0100

    remove tslint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,8 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
 npm run lint || exit 1;
-npm run tslint || exit 1;
-npm run tslint-test || exit 1;
 npm run spellcheck  || exit 1;
 
 if grep "envalfresco" . -R --exclude-dir={node_modules,.history,.idea,scripts,dist} --exclude=".env.cloud"; then
