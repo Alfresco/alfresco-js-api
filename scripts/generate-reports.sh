675ad163bd0401fedf71fe83d8206509ea7dfe64
Denys Vuika <denys.vuika@gmail.com>
2021-11-09 11:58:14 +0000

    release reports generation

---
+++ scripts/generate-reports.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+npx @alfresco/adf-cli@latest audit -d $DIR/docs/audit
+npx @alfresco/adf-cli@latest licenses -d $DIR/docs/licences
+npx @alfresco/adf-cli@alpha changelog -o $DIR/docs/changelog
