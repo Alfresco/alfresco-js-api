4da1b94c84d316571e336cabd118d01d5828b593
suneet.gupta <suneet,gupta@hyland.com>
2022-01-18 21:56:57 +0530

    Added Get Security Group JS-API

--- test/governance-services/securityGroupApi.spec.ts
+++ test/governance-services/securityGroupApi.spec.ts
@@ -1,12 +1,13 @@
 import { AlfrescoApi } from '../../src/alfrescoApi';
 import { SecurityGroupsApi } from '../../src/api/gs-classification-rest-api';
+import { expect } from 'chai';
 
 describe('Security Group API test', () => {
     let securityGroupApi: SecurityGroupsApi;
     beforeEach(async () => {
         const hostEcm = 'http://127.0.0.1:8080';
         const alfrescoApi = new AlfrescoApi({
-            hostBpm: hostEcm,
+            hostEcm: hostEcm,
             provider: 'ECM'
         });
         securityGroupApi = new SecurityGroupsApi(alfrescoApi);
@@ -14,6 +15,11 @@ describe('Security Group API test', () => {
     });
 
     it('get Security Group', async () => {
-        await securityGroupApi.getSecurityGroups("inUse ",0,10);
+        await securityGroupApi.getSecurityGroups('inUse ', 0, 10)
+            .then(data => {
+                expect(data.list.pagination.count).to.be.equal(1);
+                expect(data.list.entries[0].entry.id).to.be.equal('classification');
+                expect(data.list.entries[0].entry.groupType).to.be.equal('HIERARCHICAL');
+            });
     });
 });
