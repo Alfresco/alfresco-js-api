42195407342f565f42f1a85060605332620ad6de
Mikolaj Serwicki <mikolaj.serwicki@hyland.com>
2022-08-22 13:58:20 +0000

    feat: wip

--- src/api-clients/http-client.interface.ts
+++ src/api-clients/http-client.interface.ts
@@ -15,6 +15,11 @@
  * limitations under the License.
  */
 
+import { Authentication } from '../authentication/authentication';
+import type { Emitter } from 'event-emitter';
+
+export type { Emitter } from 'event-emitter';
+
 export interface RequestOptions {
     path: string;
     httpMethod?: string;
@@ -36,7 +41,8 @@ export interface HttpClientConfig {
     host?: string; // Should be mandatory but can't make it because of AlfrescoApiConfig incompatibility 😕
     servicePath?: string; // Should be mandatory but can't make it because of AlfrescoApiConfig incompatibility 😕
 }
-export interface HttpClient {
+
+export interface LegacyHttpClient {
     basePath: string;
     config: HttpClientConfig;
 
@@ -76,4 +82,36 @@ export interface HttpClient {
         contextRoot?: string,
         responseType?: string
     ): Promise<any>;
+}
+
+export interface SecurityOptions {
+    readonly isBpmRequest: boolean;
+    readonly enableCsrf?: boolean;
+    readonly withCredentials?: boolean;
+    readonly authentications: Authentication;
+    readonly defaultHeaders: Record<string, string>;
+}
+
+export interface HttpRequestOptions {
+    readonly path: string;
+    readonly httpMethod?: string;
+    readonly pathParams?: any;
+    readonly queryParams?: any;
+    readonly headerParams?: any;
+    readonly formParams?: any;
+    readonly bodyParam?: any;
+    readonly contentType?: string;
+    readonly accept?: string;
+    readonly returnType?: any;
+    readonly contextRoot?: string;
+    readonly responseType?: string;
+    readonly url?: string;
+}
+
+export interface HttpClient {
+    request<T = any>(url: string, options: HttpRequestOptions, security: SecurityOptions, eventEmitter: Emitter, errorEmitter: Emitter): Promise<T>;
+    post<T = any>(url: string, options: HttpRequestOptions, security: SecurityOptions, eventEmitter: Emitter, errorEmitter: Emitter): Promise<T>;
+    put<T = any>(url: string, options: HttpRequestOptions, security: SecurityOptions, eventEmitter: Emitter, errorEmitter: Emitter): Promise<T>;
+    get<T = any>(url: string, options: HttpRequestOptions, security: SecurityOptions, eventEmitter: Emitter, errorEmitter: Emitter): Promise<T>;
+    delete<T = void>(url: string, options: HttpRequestOptions, security: SecurityOptions, eventEmitter: Emitter, errorEmitter: Emitter): Promise<T>;
 }
--- test/alfrescoApiClient.spec.ts
+++ test/alfrescoApiClient.spec.ts
@@ -1,5 +1,6 @@
-import { AlfrescoApiClient, AlfrescoApiCompatibility as AlfrescoApi, AlfrescoApiConfig, FormValueRepresentation } from '../index';
-import { DateAlfresco } from  '../index';
+import { AlfrescoApiCompatibility as AlfrescoApi, AlfrescoApiConfig, FormValueRepresentation } from '../index';
+import { DateAlfresco } from '../index';
+import { SuperagentHttpClient } from '../src/superagentHttpClient';
 import { EcmAuthMock } from '../test/mockObjects';
 
 const chai = require('chai');
@@ -7,10 +8,8 @@ const expect = require('chai').expect;
 chai.use(require('chai-datetime'));
 
 describe('Alfresco Core API Client', () => {
-
     describe('type conversion', () => {
-
-        const client = new AlfrescoApiClient();
+        const client = new SuperagentHttpClient();
 
         it('should create a request with response type blob', () => {
             const queryParams = {};
@@ -22,9 +21,33 @@ describe('Alfresco Core API Client', () => {
             const responseType = 'blob';
             const url = '/fake-api/enterprise/process-instances/';
             const httpMethod = 'GET';
+            const securityOptions = {
+                isBpmRequest: false,
+                enableCsrf: false,
+                withCredentials: false,
+                authentications: {
+                    basicAuth: {
+                        ticket: '',
+                    },
+                    type: 'basic',
+                },
+                defaultHeaders: {},
+            };
 
-            const response = client.buildRequest(httpMethod, url, queryParams, headerParams, formParams, null,
-                                               contentTypes, accepts, responseType, null, null);
+            const response: any = client['buildRequest'](
+                httpMethod,
+                url,
+                queryParams,
+                headerParams,
+                formParams,
+                null,
+                contentTypes,
+                accepts,
+                responseType,
+                null,
+                null,
+                securityOptions
+            );
 
             expect(response.url).equal('/fake-api/enterprise/process-instances/');
             expect(response.header.Accept).equal('application/json');
@@ -38,7 +61,7 @@ describe('Alfresco Core API Client', () => {
             authResponseEcmMock.get201Response();
 
             const alfrescoJsApi = new AlfrescoApi({
-                hostEcm: 'http://127.0.0.1:8080'
+                hostEcm: 'http://127.0.0.1:8080',
             } as AlfrescoApiConfig);
 
             alfrescoJsApi.login('admin', 'admin').then(() => {
@@ -49,7 +72,6 @@ describe('Alfresco Core API Client', () => {
     });
 
     describe('date parsing', () => {
-
         it('should convert dates falling in GMT without a timezone', () => {
             expect(DateAlfresco.parseDate('2015-11-17T03:33:17')).to.equalTime(new Date(Date.UTC(2015, 10, 17, 3, 33, 17)));
         });
@@ -85,31 +107,27 @@ describe('Alfresco Core API Client', () => {
         it('should convert dates with a timezone with hours only', () => {
             expect(DateAlfresco.parseDate('2015-11-17T03:33:17+02')).to.equalTime(new Date(Date.UTC(2015, 10, 17, 1, 33, 17)));
         });
-
     });
 
     describe('Deserializes', () => {
-
         it('should the deserializer return an array of object when the response is an array', () => {
-            const client = new AlfrescoApiClient();
             const data = {
                 body: [
                     {
                         id: '1',
-                        name: 'test1'
+                        name: 'test1',
                     },
                     {
                         id: '2',
-                        name: 'test2'
-                    }
-                ]
+                        name: 'test2',
+                    },
+                ],
             };
-            const result = client.deserialize(data, FormValueRepresentation);
+            const result = SuperagentHttpClient['deserialize'](data, FormValueRepresentation);
             const isArray = Array.isArray(result);
-            const isObject = (result[0] instanceof (FormValueRepresentation));
+            const isObject = result[0] instanceof FormValueRepresentation;
             expect(isArray).to.equal(true);
             expect(isObject).to.equal(true);
         });
     });
-
 });
