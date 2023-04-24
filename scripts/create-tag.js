9b1741e96e5633ecb257d99230aaa848f74b3d69
alfresco-build <ardit.domi@hyland.com>
2023-04-24 09:16:12 +0200

    Test the tag creation

--- scripts/create-tag.js
+++ scripts/create-tag.js
@@ -5,14 +5,28 @@ module.exports = async ({github, context, version}) => {
     console.log('version');
     console.log(version);
 
+    console.log('Test the tag');
 
-    const organization = 'Alfresco';
+    const tagName = 'test-2';
+    const HEAD_SHA = 'c5e7db4e0d869b2d67b9bb6fe2344770c858e4ac';
 
-    const { data: availablePackages } = await github.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
-        package_type: 'npm',
-        package_name: 'adf-core',
-        org: organization
+    const createdTag = await github.rest.git.createTag({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        tag: tagName,
+        message: tagName,
+        object: HEAD_SHA,
+        type: "commit"
     });
 
-    console.log(availablePackages[0])
+    const createdRef = await github.rest.git.createRef({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: 'refs/tags/' + tagName,
+        sha: createdTag.data.sha
+    });
+
+    if (createdRef.status === 201) {
+        console.log(`Tag ${tagName} created successfully`);
+    }
 }
