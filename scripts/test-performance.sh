b8d3e4c4e6be3e3c6a3c8895a47301cbd318291b
Denys Vuika <denys.vuika@gmail.com>
2023-05-03 10:59:07 +0100

    replace ng8 with ng15 project

--- scripts/test-performance.sh
+++ scripts/test-performance.sh
@@ -7,11 +7,12 @@ cd $DIR/..
 npm run build
 
 # Test Angular build size
-cd $DIR/../test/performance/test-angular
+
+cd $DIR/../test/performance/angular-app
 npm install
 
-mkdir -p $DIR/../test/performance/test-angular/node_modules/@alfresco/js-api
-cp -R $DIR/../dist/* $DIR/../test/performance/test-angular/node_modules/@alfresco/js-api
+mkdir -p $DIR/../test/performance/angular-app/node_modules/@alfresco/js-api
+cp -R $DIR/../dist/* $DIR/../test/performance/angular-app/node_modules/@alfresco/js-api
 
 npm run build:prod || exit 1
 
--- test/performance/angular-app/src/app/app.component.ts
+++ test/performance/angular-app/src/app/app.component.ts
@@ -1,20 +1,3 @@
-/*!
- * @license
- * Copyright © 2005-2023 Hyland Software, Inc. and its affiliates. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { Component, OnInit } from '@angular/core';
 import { AlfrescoApi, DiscoveryEntry, DiscoveryApi } from '@alfresco/js-api';
 
@@ -24,7 +7,6 @@ import { AlfrescoApi, DiscoveryEntry, DiscoveryApi } from '@alfresco/js-api';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
   alfrescoApi: AlfrescoApi;
   title = 'My test performance app';
   ecmVers: any = {};
@@ -48,5 +30,4 @@ export class AppComponent implements OnInit {
       this.ecmVers = ecmVers;
     });
   }
-
 }
