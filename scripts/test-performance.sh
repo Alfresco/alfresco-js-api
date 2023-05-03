84b47dca1987335a9c311f89400583c1e8374b39
Denys Vuika <denys.vuika@gmail.com>
2023-05-03 11:58:20 +0100

    switch bundle size inspector

--- scripts/test-performance.sh
+++ scripts/test-performance.sh
@@ -9,19 +9,19 @@ npm run build
 # Test Angular build size
 
 cd $DIR/../test/performance/angular-app
-npm install
+npm install && \
 
-mkdir -p $DIR/../test/performance/angular-app/node_modules/@alfresco/js-api
-cp -R $DIR/../dist/* $DIR/../test/performance/angular-app/node_modules/@alfresco/js-api
+mkdir -p $DIR/../test/performance/angular-app/node_modules/@alfresco/js-api && \
+cp -R $DIR/../dist/* $DIR/../test/performance/angular-app/node_modules/@alfresco/js-api && \
 
 npm run build:prod || exit 1
 
 # Test webpack build size
 
 cd $DIR/../test/performance/test-webpack
-npm install
+npm install && \
 
-mkdir -p $DIR/../test/performance/test-webpack/node_modules/@alfresco/js-api
-cp -R $DIR/../dist/* $DIR/../test/performance/test-webpack/node_modules/@alfresco/js-api
+mkdir -p $DIR/../test/performance/test-webpack/node_modules/@alfresco/js-api && \
+cp -R $DIR/../dist/* $DIR/../test/performance/test-webpack/node_modules/@alfresco/js-api && \
 
 npm run build || exit 1
